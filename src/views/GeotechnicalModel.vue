de051d7d57ebb51def0d41f7dca3bf4a3da1dcbd
MilanK982 <kandic.milan@gmail.com>
2025-05-05 15:44:46 +0200

    ispravka importa u Vue modelima

--- src/views/GeotechnicalModel.vue
+++ src/views/GeotechnicalModel.vue
@@ -16,7 +16,8 @@
   </template>
   
   <script setup>
-  import { TabView, TabPanel } from 'primevue/tabview';
+  import TabView from 'primevue/tabview'; // Ispravljen uvoz
+  import TabPanel from 'primevue/tabpanel'; // Ispravljen uvoz
   import LayerTable from '../components/geotechnical/LayerTable.vue';
   import GroundWater from '../components/geotechnical/GroundWater.vue';
   import CptTable from '../components/geotechnical/CptTable.vue';
