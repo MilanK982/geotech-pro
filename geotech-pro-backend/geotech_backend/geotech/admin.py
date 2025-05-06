088bca33caede0c29ebe55c183c130351077189d
MilanK982 <kandic.milan@gmail.com>
2025-05-06 13:50:44 +0200

    registrovanje modela u admin-u

--- geotech-pro-backend/geotech_backend/geotech/admin.py
+++ geotech-pro-backend/geotech_backend/geotech/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import GeotechnicalModel, Project, Object, Layer, CptTest, CptData
 
+admin.site.register(GeotechnicalModel)
+admin.site.register(Project)
+admin.site.register(Object)
+admin.site.register(Layer)
+admin.site.register(CptTest)
+admin.site.register(CptData)
 # Register your models here.
